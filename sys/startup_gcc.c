6eba60ce9299ac006da28dd1dc39dc0590867678
Jeremy Munson <jkmunson@portalkerr.info>
2023-10-03 14:27:58 -0700

    Add interrupt handler for port f

--- sys/startup_gcc.c
+++ sys/startup_gcc.c
@@ -86,7 +86,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // Analog Comparator 2
     IntDefaultHandler,                      // System Control (PLL, OSC, BO)
     IntDefaultHandler,                      // FLASH Control
-    IntDefaultHandler,                      // GPIO Port F
+    PORTF_int_handler,                      // GPIO Port F
     IntDefaultHandler,                      // GPIO Port G
     IntDefaultHandler,                      // GPIO Port H
     IntDefaultHandler,                      // UART2 Rx and Tx
