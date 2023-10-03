b4d143b51cf5235bfa9df111eedd8e8e4e419f27
Jeremy Munson <jkmunson@portalkerr.info>
2023-10-03 14:57:31 -0700

    day

---
+++ src/sw1_int.c
@@ -0,0 +1,11 @@
+#include "common/tm4c123gh6pm.h"
+#include "setup.h"
+
+void PORTF_int_handler(void){
+	//Disable interrupt
+	//Clear
+	
+	//task
+	
+	//reenable interrupt
+}
---
+++ src/sw1_int.h
@@ -0,0 +1,2 @@
+
+void PORTF_int_handler(void);
