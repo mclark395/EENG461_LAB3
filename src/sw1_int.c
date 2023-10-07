0d68f1dd2268d35c9c55364e99ad4c0df59b2dfa
Jeremy Munson <portalkerrwebsite@gmail.com>
2023-10-06 22:47:49 -0700

    Complete Code

--- src/sw1_int.c
+++ src/sw1_int.c
@@ -26,28 +26,23 @@ void PORTF_int_handler(void){
 }
 
 void sw1_debounce(void){
-	// static = persist between calls to function, is this context.
 	static int32_t uptime_last = 0; 
 	static int32_t cycles_last = 0;
 	static enum { RELEASED, PRESSED } button_state = RELEASED;
 	
-	// Since we are triggering on both rising and falling edges, we need to determine which it was.
 	const enum { FALLING, RISING } edge_type = (GPIO_PORTF_DATA_BITS_R[SW1_PIN] == SW1_PIN);
 	
 	int32_t uptime_temp;
 	int32_t cycles_temp;
 	
 	// Early exit for don't-care state combinations
-	// Recall, button pressed = falling edge
 	if(	((button_state == PRESSED)  && (edge_type == FALLING )) ||
 		((button_state == RELEASED) && (edge_type == RISING)) ) {
 		return;
 	}
 	
-	// Atomic read of what time it is. 
-	// There is an edge case if TIMER0A has overflowed since we entered the ISR.
-	// There is an edge case if TIMER0A has overflowed after we check if it has, and before we read the timer value
-	__asm("CPSID    I\n");
+	//Atomic read of the current time
+	__asm("CPSID I\n");
 	do {
 		if(TIMER_ISR_IS_PENDING) timerISR();
 		
@@ -56,22 +51,16 @@ void sw1_debounce(void){
 		
 	// If the counter overflowed during this code block, then our reads of uptime and cycles are invalid. Re-do them.
 	} while (TIMER_ISR_IS_PENDING); 
-	__asm("CPSIE    I\n");
+	__asm("CPSIE I\n");
 	
 	int32_t seconds_passed = (uptime_temp - uptime_last);
-	// Edge case: If too many seconds had passed then the next calculation would overflow. 
-	// After 2 full seconds, enough time has passed, so just clamp this instead of using larger integers.
-	if(seconds_passed > 2) seconds_passed = 2; 
-	
-	//This is the one, true, correct calculation of how long, in cycles, it has been.
+	if(seconds_passed > 2) seconds_passed = 2; // Prevent overflow
 	int32_t cycles_passed  = (cycles_temp - cycles_last) + seconds_passed*CYCLES_PER_SEC;
 
 	if(cycles_passed > MIN_CLOCKS_DEBOUNCE) {
 		uptime_last = uptime_temp;
 		cycles_last = cycles_temp;
 		
-		//Recall that certain combinations of button states already resulted in early-exit.
-		//If made it here then the only possibilities are (PRESSED & RISING) or (RELEASED & FALLING)
 		switch(button_state) {
 			case PRESSED:
 				button_state = RELEASED;
